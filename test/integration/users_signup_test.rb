811e0f82a36e384b9375cc8c0502f3ec2b745b9d
Arthur Vieira <arthur.apv@gmail.com>
2016-02-02 01:44:51 -0200

    use SSL and Puma in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,6 +21,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 					    password_confirmation: "password" }
 		end
 		assert_template 'users/show'
-		assert_select "div.alert"
+		assert_not flash.empty?
 	end
 end
