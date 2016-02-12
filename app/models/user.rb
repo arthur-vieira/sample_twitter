4bbcc2682b818d841e67e37ed9da952f352ee233
Arthur Vieira <arthur.apv@gmail.com>
2016-02-12 02:29:51 -0200

    Finish user edit, update, index and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
 		uniqueness: { case_sensitive: false }
-	validates :password, presence: true, length: { minimum: 6 }
+	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true # has_secure_password includes a separate presence validation that specifically catches nil passwords
 	has_secure_password
 	# the method above gives:
 	# - the ability to save a "password_digest" attribute to the database. This method hashes the password with bcrypt gem;
